0dd1feea96e6b28bbac87f52adf8a0ca8bba0a63
Dragnansia <Romuald_auc@hotmail.fr>
2021-10-25 13:31:46 +0200

    add all project

---
+++ README.md
@@ -0,0 +1,14 @@
+# Kitty-Nautilus
+Kitty-Nautilus is a extension for nautilus to open kitty on current directory like gnome-terminal by default
+
+# Installation
+
+```sh
+wget -qO- https://raw.githubusercontent.com/Dragnansia/Kitty-Nautilus/main/install.sh | bash
+```
+
+# Remove
+
+```sh
+rm -f ~/.local/share/nautilus-python/extensions/kitty-nautilus.py
+```
---
+++ kitty-nautilus.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from gi import require_version
+require_version('Gtk', '3.0')
+require_version('Nautilus', '3.0')
+
+from gi.repository import Nautilus, GObject
+import os, subprocess
+
+PROCESSNAME = 'kitty'
+
+class AlacrittyExtension(Nautilus.MenuProvider, GObject.GObject):
+    def __init__(self):
+        pass
+
+    def launch_alacritty(self, menu: Nautilus.MenuItem, files):
+        path = ''
+        args = '--working-directory'
+
+        for file in files:
+            dirpath = file.get_location().get_path()
+            if os.path.isdir(dirpath) and os.path.exists(dirpath):
+                path = dirpath
+
+        subprocess.Popen([PROCESSNAME, args, path], shell=False)
+
+    def get_file_items(self, window, files):
+        item = Nautilus.MenuItem(
+            name="AlacrittyOpen",
+            label="Ouvrir dans Alacritty",
+            tip="Ouvre le dossier sélectionné dans alacritty"
+        )
+        item.connect('activate', self.launch_alacritty, files)
+        return [item]
+
+    def get_background_items(self, window, file_):
+        item = Nautilus.MenuItem(
+            name="AlacrittyOpenBackGround",
+            label="Ouvrir dans Alacritty",
+            tip="Ouvre le dossier courant dans alacritty"
+        )
+        item.connect('activate', self.launch_alacritty, [file_])
+        return [item]
